cff3e8bfc7f381f6764d9d24ce5a070ff6eae6f0
Henk Rijneveld <henk@henkrijneveld.nl>
2019-04-05 16:28:21 +0200

    Models installed from google drive

--- installpi/install-part1.sh
+++ installpi/install-part1.sh
@@ -15,12 +15,18 @@
 #	when done:
 #	swapoff /dev/sdx
 
+#
+# This fileid must be taken from the shareble link from the google drive
+export fileid=11Yw_Qdk8AzRFlcmnUlVd0ExjK9TLTbzJ
+
+
 sudo apt-get update
 sudo apt-get install -y nano
 sudo apt-get install -y libblas-dev liblapack-dev python-dev libatlas-base-dev gfortran python-setuptools git
 sudo apt-get install -y python2-pip
 sudo apt-get install -y libportaudio2 portaudio19-dev 
 sudo apt-get install -y mosquitto
+sudo apt-get install -y mosquitto-clients
 sudo apt-get install -y libhdf5-serial-dev
 sudo apt-get install -y libhdf5-dev
 sudo apt-get install -y python-h5py
@@ -29,6 +35,18 @@ sudo apt-get install -y g++
 sudo apt-get install -y alsa-base alsa-utils
 sudo apt-get install -y libasound2-dev
 sudo apt-get install -y llvm
+
+# google drive for model. Will probably change in future
+cd ../devicehive-dev
+rm -rf models
+export filename=models.zip
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+wget --load-cookies cookies.txt -O $filename \
+     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
+rm -f confirm.txt cookies.txt
+mkdir models
+unzip models.zip -d models
 
 echo " "
 echo "*****************************************"

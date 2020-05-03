fa26664dfe7444a096e1baac2958f546140acf21
Sensemaker <gijs.mos@matanga.nl>
2020-05-03 20:36:25 +0200

    bug fix in install1

--- install/pi/python3/install-part1.sh
+++ install/pi/python3/install-part1.sh
@@ -26,17 +26,17 @@ fi
 
 echo Get vggish convolution model from Google drive.
 cd ../../../devicehive-dev
-rm -rf models
+rm -rf models/vggish
 export filename=models.zip
 wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
 		| sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
 wget --load-cookies cookies.txt -O $filename \
 		 'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 rm -f confirm.txt cookies.txt
-mkdir models
-unzip models.zip -d models
+mkdir models/vggish
+unzip models.zip -d models/vggish
 echo Throw away old reenforcement model
-rm -f models.zip models/model.ckpt* models/class_labels_indices_amsterdam2.csv
+rm -f models.zip models/vggish/model.ckpt* models/vggish/class_labels_indices_amsterdam2.csv
 
 echo " "
 echo "*****************************************"
